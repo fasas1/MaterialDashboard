0909619399f79f1aad3113f07afc6afb2b4996fb
Fasasi Qasim <fasaqa1000@gmail.com>
2024-10-17 15:20:58 +0100

    pushing to remote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from "./components/Dashboard/Dashboard";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid gap-4 p-4 grid-cols-[220px,_1fr]">   
+       <Sidebar/>
+       <Dashboard/> 
     </div>
   );
-}
+} 
 
 export default App;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import AccountToggle from './AccountToggle'
+import Search  from "./Search";
+import { RouteSelect } from './RouteSelect';
+
+const Sidebar = () => {
+  return (
+    <div>
+     <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
+        <AccountToggle/>
+        <Search/>
+        <RouteSelect/>
+    </div> 
+
+
+
+
+    </div>
+  )
+}
+
+export default Sidebar
